2e3bf94288fecb11d2874bdcbc6ea601ab967522
sarun87@gmail.com <sarun87@gmail.com@6314fb0d-9e6e-2811-3c0e-943e5d19798d>
2012-03-08 00:50:59 +0000

    added return values and default constant DEFAULT_ATTR

--- trunk/src/mysched_attr.c
+++ trunk/src/mysched_attr.c
@@ -3,17 +3,21 @@
 
 int mythread_attr_init(mythread_attr_t *attr)
 {
-	attr->attr = 10;
+	attr->attr = DEFAULT_ATTR;
+	return 0;
 }
 int mythread_attr_destroy(mythread_attr_t *attr)
 {
-	//TODO: set a negative (invalid) value?
+	attr->attr = DEFAULT_ATTR;
+	return 0;
 }
 int mythread_attr_getschedparam(const mythread_attr_t *attr, struct sched_param *param)
 {
 	param->__sched_priority = attr->attr;
+	return 0;
 }
 int mythread_attr_setschedparam(mythread_attr_t *attr, const struct sched_param *param)
 {
 	attr->attr = param->__sched_priority;
+	return 0;
 }

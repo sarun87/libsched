6edd0024fe0a97960e91053cacefcac726f273b6
sarun87@gmail.com <sarun87@gmail.com@6314fb0d-9e6e-2811-3c0e-943e5d19798d>
2012-03-10 23:20:35 +0000

    Changed code to match the description given by TA

--- trunk/src/mysched.c
+++ trunk/src/mysched.c
@@ -1,34 +1,90 @@
 #include "mysched.h"
 
-void preEmptorFunction(int signum)
+int mythread_attr_init(mythread_attr_t *attr)
 {
-
+	attr->attr = DEFAULT_ATTR;
+	return 0;
+}
+int mythread_attr_destroy(mythread_attr_t *attr)
+{
+	attr->attr = -1;
+	return 0;
+}
+int mythread_attr_getschedparam(const mythread_attr_t *attr, struct sched_param *param)
+{
+	param->__sched_priority = attr->attr;
+	return 0;
+}
+int mythread_attr_setschedparam(mythread_attr_t *attr, const struct sched_param *param)
+{
+	attr->attr = param->__sched_priority;
+	return 0;
 }
 
-void alarmHandlerFunction(int signum)
+int mythread_scheduler()
 {
-	struct itimerval value;
-	getitimer(ITIMER_REAL,&value);
-
-	mythread_scheduler();
-	setitimer(ITIMER_REAL,&value,NULL);
+	mythread_t mytcb = mythread_self();
+	if(mytcb->reschedule == 1)
+	{
+		mytcb->reschedule = 0;
+		mythread_queue_t readyQueue = *mythread_readyq();
+		if(readyQueue!=NULL)
+		{
+			mythread_t highestPrio = mythread_queue_deq_prio(&readyQueue);
+			if((highestPrio->attribute)->attr  <= (mytcb->attribute)->attr)
+			{
+				// Preempt this thread.
+				mythread_block(&readyQueue, NULL);
+				return 0;
+			}
+		}
+	}
+	mythread_leave_kernel_nonpreemptive();
+	return -1;
 }
 
+void mythread_leave_kernel()
+{
+	mythread_t mytcb = mythread_self();
+	if(mytcb->reschedule == 1)
+	{
+		mytcb->reschedule = 0;
+		mythread_scheduler();
+	}
+	else
+	{
+		mythread_leave_kernel_nonpreemptive();
+	}
+}
 
 static void mythread_sighandler(int sig, siginfo_t *siginfo, void *ucp)
 {
-	if (sig == SIGALRM)
-	{
+		mythread_t mytcb = mythread_self();
 		struct itimerval value;
 		getitimer(ITIMER_REAL,&value);
+		
 
-		mythread_scheduler();
+		if(mythread_tryenter_kernel() == true)
+		{
+			// Send signal to all threads in run q
+			mythread_queue_t runQueueIter = *mythread_runq();
+			while(runQueueIter!=NULL)
+			{ 
+				if(runQueueIter->item != mytcb){
+					tgkill(-1, ((mythread_t)runQueue->item)->tid,SIGUSR1);
+				}
+				runQueueIter = runQueueIter->next;
+			}
+			mytcb->rechedule = 1;
+			// Call scheduler
+			mythread_scheduler();
+		}
+		else
+		{
+			// If unable to enter the kernel, set the reschedule flag and return
+			mytcb->rechedule = 1;
+		}
 		setitimer(ITIMER_REAL,&value,NULL);
-	}
-	else if (sig == SIGUSR1)
-	{
-		//TODO
-	}
 }
 void mythread_init_sched(void)
 {
@@ -57,9 +113,9 @@ void mythread_init_sched(void)
 		sigaction(SIGALRM,alarmHandler,NULL);
 
 		value.it_interval.tv_sec = 0;
-		value.it_interval.tv_usec = 1000;
+		value.it_interval.tv_usec = 10000;
 		value.it_value.tv_sec = 0;
-		value.it_value.tv_usec = 1000;
+		value.it_value.tv_usec = 10000;
 
 		setitimer(ITIMER_REAL,&value,NULL);
 		_isInit = 1;
